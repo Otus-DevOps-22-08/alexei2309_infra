57fc004478324c8c6f0fd1ece61c7eec0be26c8f
alexei2309 <36379996+alexei2309@users.noreply.github.com>
2022-10-10 22:01:05 +0500

    Update install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
+ #!/bin/bash
+
+echo "---Apt update---"
 sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential  
+
+echo "---Trying to install Ruby and bundler---"
+if sudo apt install -y ruby-full ruby-bundler build-essential
+then
+echo "---Ruby and bundler istalled---"
+else
+echo "---Trying to install Ruby and bundler has not success---"
+fi
+
+ruby -v
+bundler -v
